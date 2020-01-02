ab9225ee0a3c6590933d8610fe13520b2834306e
rickster50 <richard.crapnell@outlook.com>
2020-01-02 21:31:24 +0000

    cleaning up

--- head_first_python/ch01/platform_investigation.py
+++
@@ -1,7 +0,0 @@
-import sys #load sys module from standard library
-import os #load os utils from stadard libarary
-print(sys.platform)
-print(sys.api_version)
-print(sys.version)
-print(os.getcwd())
-print(os.getenv("PATH"))
---
+++ numeric_conversion/converter.py
@@ -0,0 +1,3 @@
+
+num = input("Enter Number for conversion")
+if (m/)
